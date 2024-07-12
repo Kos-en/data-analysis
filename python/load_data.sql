84fd11c2a899951c2c9105ef7a53287d77d90b0d
Joseph Kosen <kosenrock@gmail.com>
2024-07-12 21:55:23 +0300

    sql initial commit

---
+++ python/load_data.sql
@@ -0,0 +1,3 @@
+COPY companies_for_sale
+FROM '/Users/joe/Desktop/data analysis/data/companies.csv'
+DELIMITER ',' CSV HEADER;
---
+++ python/queries.sql
@@ -0,0 +1,26 @@
+--Create companies table
+CREATE TABLE companies_for_sale
+(
+    listing_date DATE,
+    asking_price NUMERIC,
+    revenue_multiple FLOAT,
+    listing_headline TEXT,
+    listing_type VARCHAR(255),
+    total_revenue_total NUMERIC,
+    total_profit_annual NUMERIC,
+    total_growth_annual FLOAT,
+    location_country VARCHAR(255),
+    founding_date DATE,
+    description_company TEXT,
+    total_revenue NUMERIC,
+    customers VARCHAR(255),
+    key_words TEXT,
+    annual_profit NUMERIC,
+    growth_annual FLOAT,
+    tech_stack VARCHAR(255),
+    business_model TEXT,
+    competitors TEXT,
+    weekly_view NUMERIC
+)
+
+SELECT tech_stack FROM companies_for_sale
